e9403625b3a541496454b079b4bfb445ec877956
Omarhussien2 <168571456+Omarhussien2@users.noreply.github.com>
2025-11-01 19:34:09 +0200

    Update gemini.js

--- functions/gemini.js
+++ functions/gemini.js
@@ -3,7 +3,15 @@ export async function onRequestPost(context) {
   const { request } = context;
   const data = await request.json();
 
-  const API_KEY = "AIzaSyDqej5ljV_TgZHgZkC6JEEqw-pN08fw7Rk";
+ const res = await fetch(
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + AIzaSyDqej5ljV_TgZHgZkC6JEEqw-pN08fw7Rk,
+  {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  }
+);
+
 
   const res = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + API_KEY, {
     method: "POST",
